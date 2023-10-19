3785caec60569b4c42c9f9679b579adc8826ca21
Kaleb Weaver <kcweaver2@bsu.edu>
2023-10-19 04:46:29 -0400

    Add grappling hook and world

---
+++ baby/baby.gd
@@ -0,0 +1,83 @@
+extends CharacterBody2D
+
+const JUMP_FORCE = 1550			
+const MOVE_SPEED = 500			
+const GRAVITY = 60				
+const MAX_SPEED = 2000			
+const FRICTION_AIR = 0.95		
+const FRICTION_GROUND = 0.85	
+const CHAIN_PULL = 105
+
+
+
+var chain_velocity := Vector2(0,0)
+var can_jump = false		
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+		
+			$chain.shoot(event.position - get_viewport().size * 0.5)
+
+		else:
+		
+			$chain.release()
+
+
+func _physics_process(_delta: float) -> void:
+
+	var walk = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * MOVE_SPEED
+	
+	velocity.y += GRAVITY
+
+	
+	if $chain.hooked:
+	
+		chain_velocity = to_local($chain.tip).normalized() * CHAIN_PULL
+		if chain_velocity.y > 0:
+			
+			chain_velocity.y *= 0.55
+		else:
+			
+			chain_velocity.y *= 1.65
+		if sign(chain_velocity.x) != sign(walk):
+			
+			chain_velocity.x *= 0.7
+	else:
+	
+		chain_velocity = Vector2(0,0)
+	velocity += chain_velocity
+
+	velocity.x += walk
+	set_velocity(velocity)
+	set_up_direction(Vector2.UP)
+	move_and_slide()
+	velocity.x -= walk
+	
+
+	
+	velocity.y = clamp(velocity.y, -MAX_SPEED, MAX_SPEED)	
+	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+	var grounded = is_on_floor()
+	if grounded:
+		velocity.x *= FRICTION_GROUND	
+		can_jump = true 
+		if velocity.y >= 5:		
+			velocity.y = 5		
+	elif is_on_ceiling() and velocity.y <= -5:	
+		velocity.y = -5
+
+
+	if !grounded:
+		velocity.x *= FRICTION_AIR
+		if velocity.y > 0:
+			velocity.y *= FRICTION_AIR
+
+	
+	if Input.is_action_just_pressed("jump"):
+		if grounded:
+			velocity.y = -JUMP_FORCE	
+		elif can_jump:
+			can_jump = false	
+			velocity.y = -JUMP_FORCE
---
+++ chain/chain.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+@onready var links = $links
+var direction := Vector2(0,0)
+var tip := Vector2(0,0)
+
+
+const SPEED = 50
+
+var flying = false
+var hooked = false
+
+func shoot(dir: Vector2) -> void:
+	direction = dir.normalized()	
+	flying = true					
+	tip = self.global_position		
+
+
+func release() -> void:
+	flying = false	
+	hooked = false	
+
+
+func _process(_delta: float) -> void:
+	self.visible = flying or hooked
+	if not self.visible:
+		return
+	var tip_loc = to_local(tip)
+
+	links.rotation = self.position.angle_to_point(tip_loc) - deg_to_rad(270)
+	$tip.rotation = self.position.angle_to_point(tip_loc) - deg_to_rad(270)
+	links.position = tip_loc
+	links.region_rect.size.y = tip_loc.length()
+
+
+func _physics_process(_delta: float) -> void:
+	$tip.global_position = tip
+	if flying:
+		if $tip.move_and_collide(direction * SPEED):
+			hooked = true
+			flying = false
+	tip = $tip.global_position
